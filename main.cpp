4a475fc3f37cca15836e669cc69037fd905353e8
Pablo <pablo1031@protonmail.com>
2023-03-17 12:58:44 +0100

    Corregidas algunas palabras en inglés. Cambiada inicialización de asignación por inicialización de lista

--- main.cpp
+++ main.cpp
@@ -24,20 +24,20 @@ int main(int argc, char *argv[]) {
 	}
 
 	// Constants
-	const std::vector<System>::size_type expected_systems = argc - 1;
-	const unsigned int max_trys = 5;
+	const std::vector<System>::size_type expected_systems {static_cast<unsigned long>(argc - 1)};
+	const unsigned int max_attempts {30};
 
 	std::vector<std::string> url_list {};
 
-	for (int i = 1; i < argc; ++i) {
-		url_list.push_back("udp://:" + std::string(argv[i]));
+	for (int i {1}; i < argc; ++i) {
+		url_list.push_back("udp://:" + std::string {argv[i]});
 	}
 
 	Mavsdk mavsdk;
 
 	cout << "Establishing connection..." << endl;
 	for (std::string url : url_list) {
-		ConnectionResult connection_result = mavsdk.add_any_connection(url);
+		ConnectionResult connection_result {mavsdk.add_any_connection(url)};
 		if (connection_result == ConnectionResult::Success) {
 			cout << "Connection established on " << url << endl;
 		} else {
@@ -47,12 +47,12 @@ int main(int argc, char *argv[]) {
 		}
 	}
 
-	std::vector<System>::size_type number_of_systems = mavsdk.systems().size();
+	std::vector<System>::size_type number_of_systems {mavsdk.systems().size()};
 
 	mavsdk.subscribe_on_new_system([&mavsdk, &number_of_systems, &expected_systems]() {
 		number_of_systems = mavsdk.systems().size();
 
-		std::shared_ptr<System> s = mavsdk.systems().back();
+		std::shared_ptr<System> s {mavsdk.systems().back()};
 		cout << "New system!" << endl;
 		cout << "System: " << s->get_system_id() << endl;
 		cout << "    Is connected: " << s->is_connected() << endl;
@@ -62,10 +62,10 @@ int main(int argc, char *argv[]) {
 		cout << "Remaining systems: " << expected_systems - number_of_systems << endl;
 		});
 	
-	unsigned int trys = 0;
-	while ((trys < max_trys) and (number_of_systems < expected_systems)) {
+	unsigned int attempt {0};
+	while ((attempt < max_attempts) and (number_of_systems < expected_systems)) {
 		std::this_thread::sleep_for(std::chrono::seconds(1));
-		++trys;
+		++attempt;
 	}
 
 	if (number_of_systems < expected_systems) {
